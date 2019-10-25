ac1fe1633d871e8b5bedbcb2d0ed6c56cf0b75a5
DavidDay <dxmdxm1992@gmail.com>
2019-10-25 14:19:52 +0800

    format code

--- tampermonkey/TubeNinja.js
+++ tampermonkey/TubeNinja.js
@@ -9,29 +9,27 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     var $ = $ || window.$;
 
-	var window_url = window.location.href;
+    var window_url = window.location.href;
     var website_host = window.location.host;
     //脚本只运行在顶层窗口
-    if (window.top != window.self){
-    	return;
-	}
+    if (window.top != window.self) {
+        return;
+    }
 
     var download = {};
-    download.start = function(){
-        if (window.location.href.indexOf('tubeninja.net') > -1)
-        {
+    download.start = function () {
+        if (window.location.href.indexOf('tubeninja.net') > -1) {
             alert('Open video/audio and then click the button...');
-        } else
-        {
-            open('http://www.tubeninja.net/?url='+encodeURIComponent(window.location.href)+'&utm_source=bookmarklet&utm_medium=redirect');
+        } else {
+            open('http://www.tubeninja.net/?url=' + encodeURIComponent(window.location.href) + '&utm_source=bookmarklet&utm_medium=redirect');
         }
     }
 
-    GM_registerMenuCommand("download any video",function(){
-            	download.start();
-            });
+    GM_registerMenuCommand("download any video", function () {
+        download.start();
+    });
 })();
