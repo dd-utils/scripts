3f361e0493def77049cc5946b0ccca4f0c5debe3
xingming.dai <xingming.dai@mihoyo.com>
2020-10-20 10:24:24 +0800

    update: 更新成图文形式

--- weixinbot/meican/meican.py
+++ weixinbot/meican/meican.py
@@ -14,19 +14,29 @@ URL_NAME = "weixin_url"
 NORMAL_DESC_NAME = "normal_desc"
 UP_DESC_NAME = "up_desc"
 UP_WEEKDAY_NAME = "up_weekday"
+TARGET_URL = "target_url"
+PIC_URL = "pic_url"
 
 conf = configparser.ConfigParser()
 conf.read(os.path.join(os.getcwd(), CONFIG_NAME), encoding='utf-8')
 desc = conf.get(SECTION_NAME, NORMAL_DESC_NAME)
 if datetime.datetime.now().weekday() == conf.getint(SECTION_NAME, UP_WEEKDAY_NAME):
     desc = conf.get(SECTION_NAME, UP_DESC_NAME)
+target_url = conf.get(SECTION_NAME, TARGET_URL)
+pic_url = conf.get(SECTION_NAME, PIC_URL)
 
 headers = {"Content-Type": "application/json"}
 data = {
-    "msgtype": "text",
-    "text": {
-        "content": desc,
-        # "mentioned_list":["@all"],
+    "msgtype": "news",
+    "news": {
+       "articles" : [
+           {
+               "title" : "点餐提醒",
+               "description" : desc,
+               "url" : target_url,
+               "picurl" : pic_url
+           }
+        ]
     }
 }
 
